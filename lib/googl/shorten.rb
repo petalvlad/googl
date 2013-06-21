b9e8b5a032723a87ca088ef301553973878f6069
Denis Kuznetsov <denis@f7.ru>
2013-06-21 15:06:32 +0400

    api request really works

--- lib/googl/shorten.rb
+++ lib/googl/shorten.rb
@@ -10,12 +10,11 @@ module Googl
     #
     def initialize(long_url, api_key = nil)
       modify_headers('Content-Type' => 'application/json')
-      if api_key.nil?
-        options = {"longUrl" => long_url}.to_json
-      else
-        options = {"longUrl" => long_url, "key" => api_key}.to_json
-      end
-      resp = post(API_URL, :body => options)
+
+      url = api_key.nil? ? API_URL : "#{API_URL}?key=#{api_key}"
+      options = {"longUrl" => long_url}.to_json
+      
+      resp = post(url, :body => options)
       if resp.code == 200
         self.short_url  = resp['id']
         self.long_url   = resp['longUrl']
